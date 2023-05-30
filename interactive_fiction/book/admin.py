0a1cb10ed6d717d976d809863e17dcfcace2188b
MATwave <stanislav.matveew@yandex.ru>
2023-05-30 15:46:38 +0300

    feat:  настроил отображение в админке

--- interactive_fiction/book/admin.py
+++ interactive_fiction/book/admin.py
@@ -9,12 +9,25 @@ from .models.item import Item
 from .models.page import PageLink
 
 
-class BookProgressAdmin(admin.ModelAdmin):
+class ItemAdminMixin:
     filter_horizontal = ('items',)
+
+    def display_items(self, obj):
+        return ', '.join([item.name for item in obj.items.all()])
+
+    display_items.short_description = 'Items'
+
+
+class BookProgressAdmin(ItemAdminMixin, admin.ModelAdmin):
+    list_display = ['user', 'book', 'book_page', 'display_items']
+
+
+class BookPageAdmin(ItemAdminMixin, admin.ModelAdmin):
+    list_display = ['title', 'book', 'display_items']
 
 
 admin.site.register(Book)
-admin.site.register(BookPage, BookProgressAdmin)
+admin.site.register(BookPage, BookPageAdmin)
 admin.site.register(PageLink)
 admin.site.register(Item)
 admin.site.register(BookProgress, BookProgressAdmin)
